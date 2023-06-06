378183c2a1ae1d6f98d55a778d25cf0c94a23258
Thaisttorres <thaisttorres1991@gmail.com>
2023-06-06 14:48:43 -0300

    Resolving conflicts

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: '5',
+        candidatos:[
+            {
+                numero: '38111',
+                nome: 'Phineas',
+                partido: 'RRR',
+                fotos:[
+                    {url:'prefeito.jpg', legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Ferb',
+                partido: 'SCPP',
+                fotos:[
+                    {url:'vice-prefeito.jpg', legenda: 'Vereador'},
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: '2',
+        candidatos:[
+            {
+                numero:'41',
+                nome:'Poof',
+                partido:'PEF',
+                vice:'Ferb',
+                fotos:[
+                    {url:'Poof-presidente.png', legenda: 'Prefeito'},
+                    {url:'vice-timmy.jpg', legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero:'99',
+                nome:'Wanda',
+                partido:'PEF',
+                vice:'Pherf',
+                fotos:[
+                    {url:'Wanda.jpg', legenda: 'Prefeito'},
+                    {url:'vice-prefeito.jpg', legenda:'Vice-Prefeito',small:true}
+                ]
+            },
+        ]
+    }
+];
